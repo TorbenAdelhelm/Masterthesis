4156b6427e46dbb8d98eb424487862bd5d586b2a
TorbenAdelhelm <123873925+TorbenAdelhelm@users.noreply.github.com>
2026-09-07 21:18:17 +0200

    Export release25 Perlin sampler

--- src/subsurface_uq/sampling/__init__.py
+++ src/subsurface_uq/sampling/__init__.py
@@ -1,8 +1,26 @@
 from .base import PermeabilitySampler
 from .empirical import EmpiricalPermeabilitySampler, load_empirical_fields
+from .perlin import (
+    RELEASE25_PERLIN_DEFAULT_SEED,
+    RELEASE25_PERLIN_DOMAIN_SIZE_M,
+    RELEASE25_PERLIN_FREQUENCY,
+    RELEASE25_PERLIN_K_MAX,
+    RELEASE25_PERLIN_K_MIN,
+    RELEASE25_PERLIN_SHAPE,
+    Release25PerlinPermeabilitySampler,
+    historical_perlin_v2_field,
+)
 
 __all__ = [
     "EmpiricalPermeabilitySampler",
     "PermeabilitySampler",
+    "RELEASE25_PERLIN_DEFAULT_SEED",
+    "RELEASE25_PERLIN_DOMAIN_SIZE_M",
+    "RELEASE25_PERLIN_FREQUENCY",
+    "RELEASE25_PERLIN_K_MAX",
+    "RELEASE25_PERLIN_K_MIN",
+    "RELEASE25_PERLIN_SHAPE",
+    "Release25PerlinPermeabilitySampler",
+    "historical_perlin_v2_field",
     "load_empirical_fields",
 ]
