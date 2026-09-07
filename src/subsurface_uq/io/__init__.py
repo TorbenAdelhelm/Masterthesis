88d367825dc061f1213df0a9e795891a4ba899f8
TorbenAdelhelm <123873925+TorbenAdelhelm@users.noreply.github.com>
2026-09-07 21:18:34 +0200

    Export UQ result writer

--- src/subsurface_uq/io/__init__.py
+++ src/subsurface_uq/io/__init__.py
@@ -1,3 +1,4 @@
 from .darus import load_prepared_permeability_ensemble
+from .results import save_monte_carlo_result
 
-__all__ = ["load_prepared_permeability_ensemble"]
+__all__ = ["load_prepared_permeability_ensemble", "save_monte_carlo_result"]
