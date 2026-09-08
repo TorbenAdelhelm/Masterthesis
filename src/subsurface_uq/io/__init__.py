e7d8108c0b3b7fa66ccd09705cb2e391d1113493
TorbenAdelhelm <123873925+TorbenAdelhelm@users.noreply.github.com>
2026-09-08 12:41:15 +0200

    Export run provenance helpers

--- src/subsurface_uq/io/__init__.py
+++ src/subsurface_uq/io/__init__.py
@@ -1,4 +1,11 @@
 from .darus import load_prepared_permeability_ensemble
+from .provenance import git_head, runtime_versions, sha256_file
 from .results import save_monte_carlo_result
 
-__all__ = ["load_prepared_permeability_ensemble", "save_monte_carlo_result"]
+__all__ = [
+    "git_head",
+    "load_prepared_permeability_ensemble",
+    "runtime_versions",
+    "save_monte_carlo_result",
+    "sha256_file",
+]
